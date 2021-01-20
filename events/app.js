c72815fd6f54fc90546d6d35178d8dca54d4d2a4
vvvk-gh <vamsivutukuri1997@gmail.com>
2021-01-20 21:15:51 +0530

    mousemove events

--- events/app.js
+++ events/app.js
@@ -3,8 +3,19 @@ var app = new Vue({
     data:{
         title : 'Events',
         age:24,
+        x:0,
+        y:0
     },
     methods:{
-
+        inc : function(inc){
+            this.age += inc;
+        },
+        dec : function(dec){
+            this.age -= dec;
+        },
+        updateXY : function(event){
+            this.x = event.offsetX; 
+            this.y = event.offsetY;
+        }
     }
 })
