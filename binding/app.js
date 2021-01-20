d6766c18aeb481995f5ec818aff73e3b6c2c0e9f
vvvk-gh <vamsivutukuri1997@gmail.com>
2021-01-20 20:15:56 +0530

    Vue binding and html

---
+++ binding/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el : '#vueComp',
+    data : {
+        title : 'Vue Binding',
+        google : 'https://www.google.com',
+        websiteTag : `<a href="https://www.google.com">Click Here</a>`
+    }
+})
